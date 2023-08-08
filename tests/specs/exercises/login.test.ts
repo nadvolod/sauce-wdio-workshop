f23c60999ad0da436909c9533b984e2c99b986f6
nadvolod <nadvolod@gmail.com>
2023-08-08 14:53:11 -0400

    update exercise to not have answers

--- tests/specs/exercises/login.test.ts
+++ tests/specs/exercises/login.test.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { $, browser, expect } from '@wdio/globals'
 import { before } from 'node:test'
+import input from 'postcss/lib/input'
 
 const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!, {
     auth: {
@@ -31,49 +32,40 @@ describe('Forge 4 Application', () => {
     })
 
     describe('login', () => {
-        it('it should fail with no input', async () => {
+        it('should fail with no input', async () => {
+            // Use Docs https://webdriver.io/docs/api/browser/url
             // go to home page  
-            await browser.url('/')
+            await browser.url('FOO')
             // click login button
-            await $('aria/Login').click()
+            await $('BAR').click()
             // click login button
-            await $('form').$('aria/Login').click()
+            await $('form').$('FOO').click()
             // check for error message
-            await expect($('aria/Invalid login credentials')).toBePresent()
+            await expect($('BAR')).toBePresent()
         })
 
         it('should be invalid if username is not an email', async () => {
             // go to login page
             await browser.url('/login')  
             // get email input element
-            const input = await $('input[type="email"]')
             // set value to invalid email
-            await input.setValue('invalid')
-            // check if input is invalid
+            // check if input is invalid, use method below
             expect(await browser.execute((elem: HTMLInputElement) => elem.checkValidity(), input)).toBe(false)
-
-            await input.setValue(email)
-            expect(await browser.execute((elem: HTMLInputElement) => elem.checkValidity(), input)).toBe(true)
         })
 
         it('email should be valid with valid email', async () => {
             // go to login page
-            await browser.url('/login')
             // get email input element 
-            const input = await $('input[type="email"]')
             // set value to valid email            
-            await input.setValue(email)
             // assert that input is valid
-            expect(await browser.execute((elem: HTMLInputElement) => elem.checkValidity(), input)).toBe(true)
         })
 
-        it('should be fail if credentials are invalid', async () => {
-            await $('input[type="email"]').setValue(email)
-            await $('input[type="password"]').setValue('invalid')
-            await $('form').$('aria/Login').click()
-            await expect($('aria/Invalid login credentials')).toBePresent()
-            await $('aria/x').waitForClickable()
-            await $('aria/x').click()
+        it('should fail if credentials are invalid', async () => {
+            // go to login page
+            // set email to valid email
+            // set password to invalid password
+            // click login button
+            // check for error message
         })
 
         it('should fail if email is not confirmed', async () => {
---
+++ tests/specs/exercises/projectDetails.test.ts
@@ -0,0 +1,26 @@
+
+describe('Forge 4 Application', () => {
+    it('should load dashboard successfully with 8 cards', async () => {
+      // go to home page  
+      // check for title
+        // check for 8 cards
+    })
+
+    describe('project details', () => {
+        it('should allow to open a project', async () => {
+          // go to home page  
+          // wait for cards to load
+            // get title of first card
+            // click on first card
+            // check if title is present
+        })
+
+        it('should allow to fund a project', async () => {
+          // navigate to one project
+          // click on fund this project
+          // set value to 100
+            // click on fund button
+            // check if thanks message is present
+        })
+    })
+})
